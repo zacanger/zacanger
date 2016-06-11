6821a54184f6030719e4ed498cdda88405811a84
zacanger <zac@zacanger.com>
2016-06-11 12:38:04 -0600

    js

---
+++ zacanger.js
@@ -0,0 +1,216 @@
+const zacanger = {
+  names       : {
+    preferred : 'Zac Anger'
+  , legal     : 'Zachary Eric Unger'
+  , handle    : 'zacanger'
+  }
+
+, byline      : 'JS Dev, Nix Hacker, Musician'
+
+, status      : 'Happy employed.'
+
+, website     : 'http://zacanger.com'
+
+, writing     : 'http://blog.zacanger.com'
+
+, occupations : [
+    '*n*x nerd'
+  , 'debian administrator'
+  , 'js developer'
+  , 'web enthusiast'
+  , 'musician'
+  , 'recording engineer'
+  , 'sometime web designer'
+  , 'open source supporter'
+  , 'node fanboi'
+  ]
+}
+
+, description : `
+  I'm an enthusiastic, always-curious individual with a varied history in a
+  broad spectrum of fields. I've worked solo and in teams in a wide variety of
+  professions and environments. I'm especially interested in new technologies,
+  development, and design, with a strong preference for web technologies and
+  doing things the UNIX way. I'm passionate about building better interfaces and
+  smoother interactions, opening up the web to more people, keeping it open for
+  everyone, keeping software and development free, open, and transparent, and
+  keeping content accessible.`
+
+, projects    : [
+    {
+      name    : 'luvi'
+    , url     : 'http://npm.im/luvi'
+    , summary : 'Dev server with nifty extras and easy config and API.'
+    },{
+      name    : 'mdkb'
+    , url     : 'https://github.com/zacanger/mdkb'
+    , summary : 'Markdown-based knowledgebase webapp'
+    },{
+      name    : 'create-repo'
+    , url     : 'http://npm.im/create-repo'
+    , summary : 'Node tool to create GH repo using name/description from package.json.'
+    },{
+      name    : 'hey-you'
+    , url     : 'http://npm.im/hey-you'
+    , summary : 'A very nice little desktop widget.'
+    },{
+      name    : 'zeess'
+    , url     : 'http://zacanger.github.io/zeess'
+    , summary : 'CSS Microframework'
+    },{
+      name    : 'dolog'
+    , url     : 'http://npm.im/dolog'
+    , summary : 'Node module to turn console.log on and off at will'
+    },{
+      name    : 'hipper'
+    , url     : 'http://npm.im/hipper'
+    , summary : 'A text editor for the terminal.'
+    },{
+      name    : 'lilnote'
+    , url     : 'http://npm.im/lilnote'
+    , summary : 'A tiny note-taking app that stores notes in JSON'
+    },{
+      name    : 'scrapegh'
+    , url     : 'http://npm.im/scrapegh'
+    , summary : 'A command-line utility for scraping GitHub'
+    },{
+      name    : 'scrapingnode'
+    , url     : 'http://npm.im/scrapingnode'
+    , summary : 'A tiny command-line scraper in Node'
+    },{
+      name    : 'Pharaoh'
+    , url     : 'http://pharaoh.js.org'
+    , summary : 'An educational tool, for live broadcasting of code. This is the web frontend.'
+    },{
+      name    : 'Pharaoh-Desktop'
+    , url     : 'http://npm.im/pharaoh'
+    , summary : 'The desktop app for Pharaoh; installable as an executable from NPM.'
+    },{
+      name    : 'ayuba'
+    , url     : 'https://github.com/zacanger/ayuba.git'
+    , summary : 'A desktop Markdown editor, with realtime rendering, customisable themes, and Mongo storage.'
+    },{
+      name    : 'markvi'
+    , url     : 'http://npm.im/markvi'
+    , summary : 'In-browser Markdown editor with realtime rendering & vim-like keybinds.'
+    },{
+      name    : 'pengins'
+    , url     : 'http://npm.im/pengins'
+    , summary : 'A tiny script to give you \'pengins, in ur xterm, chillin.\''
+    },{
+      name    : 'z'
+    , url     : 'https://github.com/zacanger/z.git'
+    , summary : 'My own configuration files, scripts, cheatsheets, and manpages.'
+    },{
+      name    : 'F Me On Github'
+    , url     : 'http://zacanger.github.io/f-me-on-github/'
+    , summary : 'An amusing, inappropriate take on the \'Fork Me On Github\' ribbon.'
+    }
+  ]
+
+, links       : [
+    {
+      name    : 'Pinboard'
+    , url     : 'http://pinboard.in/u:zacanger'
+    },{
+      name    : 'LinkedIn'
+    , url     : 'http://linkedin.com/in/zacanger'
+    },{
+      name    : 'Twitter'
+    , url     : 'http://twitter.com/zacanger'
+    },{
+      name    : 'Tumblr'
+    , url     : 'http://zacanger.tumblr.com'
+    },{
+      name    : 'Github'
+    , url     : 'http://github.com/zacanger'
+    },{
+      name    : 'Soundcloud'
+    , url     : 'http://soundcloud.com/zacanger'
+    },{
+      name    : 'Bandcamp'
+    , url     : 'http://zacanger.bandcamp.com'
+    },{
+      name    : 'DeviantArt'
+    , url     : 'http://zacanger.deviantart.com'
+    },{
+      name    : 'Pen'
+    , url     : 'http://zacanger.pen.io'
+    },{
+      name    : 'Facebook'
+    , url     : 'http://facebook.com/zacangermusic'
+    },{
+      name    : 'Google+'
+    , url     : 'https://plus.google.com/+zacangermusic/'
+    }
+  ]
+
+, skills      : {
+    skilled   : [
+      'javascript'
+    , 'react'
+    , '(neo)vim'
+    , 'bash/sh'
+    , 'debian administration'
+    , 'css/stylus/sass/less'
+    , 'node'
+    , 'angular (1.x)'
+    , 'vue'
+    ]
+  , learning : [
+      'docker'
+    , 'ruby'
+    , 'python'
+    , 'haskell'
+    , 'soft skills'
+    ]
+  }
+
+, "relevant experience": [
+    {
+      name: "Jane.com",
+      "position": "Front-end developer",
+      "dates": "2016-Present",
+      "description": "Buildin' stuffz!",
+      "technologies": "React, CSS, *nix, Typescript, Koa, Express, Purescript"
+    },{
+      name: "DevMountain",
+      "position": "Mentor and Junior Developer",
+      "dates": "2016",
+      "description": "Mentored group of new devs; planned and carried out lectures; maintained curriculum; worked on internal projects",
+      "technologies": "Node, Express, Angular, Chromium (extensions), CSS and preprocessors, MongoDB and Mongoose, MySQL, build systems, Linux."
+    },{
+      name: "Mickey's Wholesale Pizza",
+      "position": "Web Developer",
+      "dates": "2014-2015",
+      "description": "Full revamp of wholesale foods company's web presence.",
+      "technologies": "HTML, CSS, JavaScript, jQuery, Twitter Bootstrap, image manipulation."
+    },{
+      name: "RLH Guitars",
+      "position": "Venue management, recording engineer",
+      "dates": "2005-2008",
+      "description": "Ran live music venue; lead engineer at in-house recording studio.",
+      "technologies": "Windows, Mac, and Linux workstations and servers; pro audio hardware and software."
+    },{
+      name: "York County Library System",
+      "position": "Volunteer",
+      "dates": "2001-2005",
+      "description": "Worked with catalogues, DDS, customer service.",
+      "technologies": "Various library systems including bespoke software and national systems."
+    }
+  ]
+
+, qualifications : [
+    'Comptia A+ Certification'
+  , 'Rackspace Cloud U Certificate'
+  , 'Microsoft Virtual Academy Certificates (Various)'
+  ]
+
+, education     : {
+    name        : 'DevMountain'
+  , completed   : 2016
+  , description : 'Full-time immersive JavaScript program with focus on MEAN Stack.'
+  , url         : 'http://devmounta.in'
+  }
+
+
