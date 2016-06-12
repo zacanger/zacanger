e4e80bde92b8b5baa70b403ed1cd1fa574da96f7
zacanger <zac@zacanger.com>
2016-06-11 21:28:08 -0600

    cln

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const
   path = require('path')
 , fs   = require('fs')
+, me   = path.resolve(__dirname, 'zacanger.json')
+, out  = process.stdout
 
-fs.createReadStream(path.resolve(__dirname, 'zacanger.json')).pipe(process.stdout)
+fs.createReadStream(me).pipe(out)
 
