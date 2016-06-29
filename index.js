535b1ae3acfabb77e755bdb7e7d5e81f410eb3d4
zacanger <zac@zacanger.com>
2016-06-29 08:45:58 -0600

    trying to get colours to work\!

--- index.js
+++ index.js
@@ -3,7 +3,42 @@
 const
   fs   = require('fs')
 , out  = process.stdout
+, sin  = process.stdin
 , path = require('path')
 , me   = path.resolve(__dirname, 'zacanger.json')
 
-fs.createReadStream(me).pipe(out)
+// fs.createReadStream(me).pipe(out)
+
+function gencolours() {
+  const colours = []
+
+  for (let i = 0; i < (6 * 7); i++) {
+    const
+      pi3 = Math.floor(Math.PI / 3)
+    , n   = (i * (1.0 / 6))
+    , r   = Math.floor(3 * Math.sin(n) + 3)
+    , g   = Math.floor(3 * Math.sin(n + 2 * pi3) + 3)
+    , b   = Math.floor(3 * Math.sin(n + 4 * pi3) + 3)
+    colours.push(36 * r + 6 * g + b + 16)
+  }
+
+  return colours
+}
+
+const rainbowColours = gencolours()
+
+let colourIndex = 0
+
+const col = str => {
+  const colour = rainbowColours[colourIndex % rainbowColours.length]
+  colourIndex += 1
+  return `\u001b[38;5;${colour}m${str}\u001b[0m`
+}
+
+sin.on('data', c => {
+  c.toString().split('').map(w => {
+    out.write(col(w))
+  })
+})
+
+fs.createReadStream(me).pipe(sin)
