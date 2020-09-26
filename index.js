2aa25e21ec861179490dadc0e2b21c518cd420be
zacanger <zac@zacanger.com>
2020-09-26 11:08:51 -0600

    chore: ts

--- index.js
+++ index.js
@@ -1,46 +1,7 @@
 #!/usr/bin/env node
 
-const colors = require('zeelib/lib/colorize').default
-
-const zac = {
-  Name: 'Zac Anger',
-  Title: 'Senior DevOps Engineer',
-  Web: 'https://zacanger.com',
-  CV: 'https://zacanger.com/cv',
-  GitHub: 'https://github.com/zacanger',
-  LinkedIn: 'https://linkedin.com/in/zacanger',
-  Email: 'zac@zacanger.com',
-}
-
-const divider = '-'.repeat(42)
-
-const infoString = `
-${colors.bold(colors.blue(divider))}
-${colors.green(zac.Name)}
-${colors.green(zac.Title)}
-${colors.blue(divider)}
-${Object.keys(zac)
-  .reduce(
-    (p, c) =>
-      ['Name', 'Title'].includes(c)
-        ? p + ''
-        : p + `${colors.bold(c)}: ${colors.yellow(zac[c])}\n`,
-    ''
-  )
-  .trim()}
-${colors.bold(colors.blue(divider))}
-`
-  .trim()
-  .split('\n')
-  .reduce((p, c) => p + `    ${c}\n`, '')
-  .trimEnd()
-
-const main = () => {
-  console.log(infoString)
-}
-
 if (module.parent) {
-  module.exports = zac
+  module.exports = require('./lib').zac
 } else {
-  main()
+  require('./lib').main()
 }
---
+++ src/index.ts
@@ -0,0 +1,39 @@
+// @ts-nocheck
+import colors from 'zeelib/lib/colorize'
+
+export const zac = {
+  Name: 'Zac Anger',
+  Title: 'Senior DevOps Engineer',
+  Web: 'https://zacanger.com',
+  CV: 'https://zacanger.com/cv',
+  GitHub: 'https://github.com/zacanger',
+  LinkedIn: 'https://linkedin.com/in/zacanger',
+  Email: 'zac@zacanger.com',
+}
+
+const divider = '-'.repeat(42)
+
+const infoString = `
+${colors.bold(colors.blue(divider))}
+${colors.green(zac.Name)}
+${colors.green(zac.Title)}
+${colors.blue(divider)}
+${Object.keys(zac)
+  .reduce(
+    (p, c) =>
+      ['Name', 'Title'].includes(c)
+        ? p + ''
+        : p + `${colors.bold(c)}: ${colors.yellow(zac[c])}\n`,
+    ''
+  )
+  .trim()}
+${colors.bold(colors.blue(divider))}
+`
+  .trim()
+  .split('\n')
+  .reduce((p, c) => p + `    ${c}\n`, '')
+  .trimEnd()
+
+export const main = () => {
+  console.log(infoString)
+}
